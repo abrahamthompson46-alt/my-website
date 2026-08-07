308fb738027270de8b462bef5ed2ae81fc8029b1
abrahamthompson46-alt <abrahamthompson46@gmail.com>
2026-08-07 10:07:07 +0000

    Fix manage.py production settings loading

--- manage.py
+++ manage.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+    """Run administrative tasks."""
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        os.getenv(
+            "DJANGO_SETTINGS_MODULE",
+            "config.settings.development",
+        ),
+    )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure your virtual environment is active."
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
